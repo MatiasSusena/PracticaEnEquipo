8e0b1a6068db4f8bd23cccd90f2a08d7f57995b9
Guillermo Gonzalez <topografia3g@hotmail.com>
2013-05-06 11:54:53 +0200

    Comentario de nombre
    He comentado el nombre

--- PruebaEquipoLosBandarras/Form1.cs
+++ PruebaEquipoLosBandarras/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+//Equipo Los Bandarras
 namespace PruebaEquipoLosBandarras
 {
     public partial class Form1 : Form
